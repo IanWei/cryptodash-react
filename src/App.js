9203420f5a43507ab7cbb30676c379c8c18a57a1
Ian Wei <ikaeuu@IanWei.lan>
2018-10-24 16:26:22 +0800

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const CustomElement = styled.div`
+  color: green;
+  font-size: 30px;
+`;
+const BlueElement = styled(CustomElement)`
+  color: blue;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>Hello World!</p>
+      <div>
+        <CustomElement>
+          <p>Hello World!</p>
+        </CustomElement>
+        <BlueElement>
+          Great
+        </BlueElement>
+
       </div>
+
     );
   }
 }
