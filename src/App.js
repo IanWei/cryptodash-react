b079cbe3b7dfeb7ed14efed5897c808975a31b3c
Ian Wei <ikaeuu@IanWei.lan>
2018-10-25 14:02:57 +0800

    finish content with dynamic control

--- src/App.js
+++ src/App.js
@@ -1,36 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Logo = styled.div`
   font-size: 1.5em;
 `;
 const ControlButton = styled.div`
+  cursor: pointer;
+  ${props => props.active && css`
+    text-shadow: 0px 0px 60px #03ff03;   
+  `}
+`;
+
+const Bar = styled.div`
+  display: grid;
+  grid-template-columns: 180px auto 100px 100px; 
+  margin-bottom: 40px;
 `;
 
 const AppLayout = styled.div`
   padding: 40px;
-  display: grid;
-  grid-template-columns: 180px auto 100px 100px; 
+`;
+
+const Content = styled.div`
+  
 `;
 
 class App extends Component {
+  state = {
+    page: 'dashboard'
+  };
+  displayingDashboard = () => this.state.page === 'dashboard';
+  displayingSettings = () => this.state.page === 'settings';
+
   render() {
+
+
     return (
       <AppLayout>
-        <Logo>
-          CyptoDash
-        </Logo>
-        <div>
-        </div>
-        <ControlButton>
-          DashBoard
-        </ControlButton>
-        <ControlButton>
-          Settings
-        </ControlButton>
-
+        <Bar>
+          <Logo>
+            CyptoDash
+          </Logo>
+          <div>
+          </div>
+          <ControlButton onClick={() => {this.setState({page: 'dashboard'})}} active={this.displayingDashboard()}>
+            DashBoard
+          </ControlButton>
+          <ControlButton onClick={() => {this.setState({page: 'settings'})}} active={this.displayingSettings()}>
+            Settings
+          </ControlButton>
+        </Bar>
+        <Content>Hello I'm {this.state.page}</Content>
       </AppLayout>
+
 
     );
   }
