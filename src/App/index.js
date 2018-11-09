d4644022ec632192e3b50974b58d055dbc7f2e08
Ian Wei <ikaeuu@IanWei.local>
2018-11-09 14:43:25 +0800

    add bar and grid

--- src/App/index.js
+++ src/App/index.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 
 
 class App extends Component {
   render() {
     return(
       <AppLayout>
+        <AppBar>
+
+        </AppBar>
         <WelcomeMessage/>
       </AppLayout>
     );
