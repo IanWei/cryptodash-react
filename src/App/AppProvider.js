1ded7e43214d2769fb30c358b6e711736309a98e
Ian Wei <ikaeuu@IanWei.lan>
2018-11-09 16:35:26 +0800

    Setting page, localStorage

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -4,13 +4,35 @@ export const AppContext = React.createContext();
 
 export class AppProvider extends Component {
   constructor(props) {
-    super(props);
+    super( props );
     this.state = {
-      page: 'settings',
-      setPage: this.setPage
+      page: 'dashboard',
+      ...this.saveSettings(),
+      setPage: this.setPage,
+      confirmFavorites: this.confirmFavorites
     }
   }
-  setPage = page => this.setState({page});
+
+  confirmFavorites = () => {
+    this.setState({
+      firstVisit: false,
+      page: 'dashboard'
+    });
+    localStorage.setItem("cryptoDash", JSON.stringify({
+      test: 'Hello'
+    }));
+  };
+
+  saveSettings() {
+    let cryptoDashData = JSON.parse( localStorage.getItem( 'cryptoDash' ) );
+    if ( !cryptoDashData ) {
+      return { page: 'settings', firstVisit: true }
+    }
+    return {};
+  }
+
+  setPage = page => this.setState( { page } );
+
   render() {
     return (
       <AppContext.Provider value={this.state}>
--- src/App/WelcomeMessage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-const WelcomeMessage = props => {
-  return <div>Welcome to CryptoDash</div>
-};
-
-export default WelcomeMessage;
