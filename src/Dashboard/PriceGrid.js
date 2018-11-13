f5057e1dd143a4c29bf889ec22bde193dc64beeb
Ian Wei <ikaeuu@IanWei.lan>
2018-11-13 15:54:51 +0800

    Add Coin Spotlight

--- src/Dashboard/PriceGrid.js
+++ src/Dashboard/PriceGrid.js
@@ -15,7 +15,7 @@ export default function() {
     <AppContext.Consumer>
       {({prices}) => (
         <PriceGrid>
-          {prices.map((price, index) => <PriceTile index={index} price={price}/>)}
+          {prices.map((price, index) => <PriceTile key={index} index={index} price={price}/>)}
         </PriceGrid>
       )}
     </AppContext.Consumer>
--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Page from '../Shared/Page';
 import PriceGrid from './PriceGrid';
+import CoinSpotlight from './CoinSpotlight';
+import styled from 'styled-components';
+
+const CharGrid = styled.div`
+  display: grid;
+  margin-top: 20px;
+  grid-gap: 15px;
+  grid-template-columns: 1fr 3fr;
+`;
 
 export default function() {
   return (
     <Page name="dashboard">
       <PriceGrid/>
+      <CharGrid>
+        <CoinSpotlight/>
+        <div>Chart goes here</div>
+      </CharGrid>
     </Page>
   );
 }
