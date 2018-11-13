a71beb5469dd8ba9a7610726dd756823beffe92e
Ian Wei <ikaeuu@IanWei.lan>
2018-11-13 14:46:10 +0800

    Dashboard Grid

--- src/App/index.js
+++ src/App/index.js
@@ -5,6 +5,7 @@ import AppBar from './AppBar';
 import { AppProvider } from './AppProvider';
 import Settings from '../Settings';
 import Content from '../Shared/Content';
+import Dashboard from '../Dashboard';
 
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
           <AppBar/>
           <Content>
             <Settings/>
+            <Dashboard/>
           </Content>
         </AppProvider>
       </AppLayout>
