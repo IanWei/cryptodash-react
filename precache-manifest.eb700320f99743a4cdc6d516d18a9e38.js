4387b6296071ac409739b999fe3b0891545cbe39
IanWei <ikaeuu@IanWei.local>
2019-02-18 13:19:05 +1100

    Updates

---
+++ precache-manifest.eb700320f99743a4cdc6d516d18a9e38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fcb4f0afeb38c66c156",
+    "url": "/cryptodash-react/static/js/runtime~main.8fcb4f0a.js"
+  },
+  {
+    "revision": "8f163af841f024a2ce6f",
+    "url": "/cryptodash-react/static/js/main.8f163af8.chunk.js"
+  },
+  {
+    "revision": "29530c9b31806a7deed5",
+    "url": "/cryptodash-react/static/js/1.29530c9b.chunk.js"
+  },
+  {
+    "revision": "8f163af841f024a2ce6f",
+    "url": "/cryptodash-react/static/css/main.8a443d23.chunk.css"
+  },
+  {
+    "revision": "6387b647a104dc64b84b416ee5da1851",
+    "url": "/cryptodash-react/index.html"
+  }
+];
